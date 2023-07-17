539275d3fa63b1b1fa4228b145fda36cfebc845f
AndiMajore <andi.majore@googlemail.com>
2023-07-17 20:33:18 +0200

    adjusted paths

--- docker/Config.js
+++ docker/Config.js
@@ -1,6 +1,6 @@
-const HOST = "apps.cosy.bio/proharmed"
-export const CONTEXT_PATH="./"
+const HOST = "apps.cosy.bio"
+export const CONTEXT_PATH="api-proharmed/"
 // export const BACKEND_PORT= "8080"
 export const HOST_URL = "https://" + HOST //+ ":" + BACKEND_PORT
-export const PATH_PREFIX= "/"
+export const PATH_PREFIX= "proharmed/"
 export const STATIC_PATH="/static/"
