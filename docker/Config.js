8b11eb13ea6676bd0d457667944e0354704b55f6
AndiMajore <andi.majore@googlemail.com>
2023-07-17 21:50:31 +0200

    updating config

--- docker/Config.js
+++ docker/Config.js
@@ -1,5 +1,5 @@
 const HOST = "apps.cosy.bio"
-export const CONTEXT_PATH="api-proharmed/"
+export const CONTEXT_PATH="/api-proharmed/"
 // export const BACKEND_PORT= "8080"
 export const HOST_URL = "https://" + HOST //+ ":" + BACKEND_PORT
 export const PATH_PREFIX= "/proharmed/"
